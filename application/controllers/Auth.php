6124152d217190a6404530051d57a231cf70577f
santo14 <susantosanto290687@gmail.com>
2022-12-20 23:49:33 +0700

    start commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,64 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+		$data['title'] = 'Login Page';
+		$this->load->view('template/auth_header', $data);
+		$this->load->view('auth/login');
+		$this->load->view('template/auth_footer');
+	}
+
+	public function registration()
+	{
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+			'is_unique' => 'This email has already registered'
+		]);
+		$this->form_validation->set_rules(
+			'password1',
+			'Password',
+			'required|trim|min_length[3]|matches[password2]',
+			[
+				'matches' => 'Password dont match',
+				'min_length' => 'Password to short'
+
+			]
+		);
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+		if ($this->form_validation->run() == false) {
+
+			$data['title'] = 'Registration Page';
+
+			$this->load->view('template/auth_header', $data);
+			$this->load->view('auth/registration');
+			$this->load->view('template/auth_footer');
+		} else {
+			$data = [
+				'name' => $this->input->post('name'),
+				'email' => $this->input->post('email'),
+				'image' => 'default.jpg',
+				'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 1,
+				'date_created' => time()
+			];
+
+			$this->db->insert('user', $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+			Congratulation, youre account has been created. Please Login</div>');
+
+			redirect('auth');
+		}
+	}
+}
